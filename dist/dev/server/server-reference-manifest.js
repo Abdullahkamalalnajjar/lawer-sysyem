cb9c4e1c566efc76c458881394ea48923f379fd9
kemo <abdallahalnajjar01@gmail.com>
2026-04-27 22:21:40 +0300

    chore: ignore dist/ build artifacts

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wD2lnlPcCoRpkMZ9X6qT/HOcw6eNLwaHjlGKhyCTY0s=\"\n}"
--- dist/dev/static/chunks/src_app_login_page_0wg7ai-.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_login_page_0dta7vn.js"],
-    source: "dynamic"
-});
